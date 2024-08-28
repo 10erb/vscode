143307241055120fa13c2e993d08bf4b83835767
10erb <tnerb.official@gmail.com>
2024-08-28 19:13:55 +0400

    this is non-funct

--- clang/c/hangman/hangman.c
+++ clang/c/hangman/hangman.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-int main(void){
-    printf("hello, mundo.");
+char lettersGuessed[];
+int guessCount;
+
+void prompt_for_guess(void);
+int main(void)
+{
+    do
+    {
+        prompt_for_guess();
+        printf("%c\n", toupper(lettersGuessed[guessCount]));
+    } while (guessCount < 10);
+}
+
+void prompt_for_guess(void)
+{
+    printf("Enter your guess: ");
+    char letter;
+    scanf("%c", &letter);
+    if (isalpha(letter) == 0)
+    {
+        printf("Please input a letter.\n");
+    }
+    ++guessCount;
+    lettersGuessed[guessCount] = letter;
 }
